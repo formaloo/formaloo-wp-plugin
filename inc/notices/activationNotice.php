3f6fa9d1b5865da254ad5102b2098c11188ff3b6
Tadeh <tadeh19@gmail.com>
2021-03-03 09:36:32 +0330

    fix activation notice issue

--- inc/notices/activationNotice.php
+++ inc/notices/activationNotice.php
@@ -10,7 +10,7 @@ class Formaloo_Activation_Class extends Formaloo_Main_Class {
 
     static function formalooActivationHook() {
         /* Create transient data */
-        set_transient( 'formaloo_admin_notice_activation', true, 5 );
+        set_transient( 'formaloo_admin_notice_activation', true, 0 );
         wp_schedule_event( time(), 'hourly', 'formaloo_sync_woocommerce' );
     }
     
@@ -18,17 +18,17 @@ class Formaloo_Activation_Class extends Formaloo_Main_Class {
      * Admin Notice on Activation.
      * @since 0.1.0
      */
-    static function formalooAdminNoticeActivationNotice(){
+    static function formalooAdminNoticeActivationNotice() {
     
         /* Check transient, if available display notice */
-        if( get_transient( 'formaloo_admin_notice_activation' ) ){
+        if (get_transient('formaloo_admin_notice_activation')) {
             ?>
             <div class="updated notice is-dismissible">
             <p><?php echo __('Thank you for using the Formaloo plugin!', 'formaloo-form-builder') ?> <a href="<?php echo admin_url( "admin.php?page=formaloo-settings-page" ) ?>"><strong><?php echo __('Get Started by visiting the Settings Page', 'formaloo-form-builder') ?></strong></a>.</p>
             </div>
             <?php
             /* Delete transient, only display this notice once. */
-            delete_transient( 'formaloo_admin_notice_activation' );
+            delete_transient('formaloo_admin_notice_activation');
         }
     }
 
